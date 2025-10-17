3a706c7cc6d107fdd04ef1c45da8d990b016d215
johnsonluu517 <luujohnson07@gmail.com>
2025-10-17 21:47:08 +0000

    finished problem 1, working on problem 2

--- Main.java
+++ Main.java
@@ -4,6 +4,33 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // write solutions to problems here
+    Scanner scan = new Scanner(System.in);
+    int input1 = 0;
+    int sum = 0;
+    while (input1 >= 0)
+    {
+      System.out.println("Type a Number, no negatives, input -1 to add sum of all numbers");
+     input1 = scan.nextInt();
+      sum = sum + input1;
+    }
+    sum++;
+    System.out.println("Sum is " + sum);
+    
+    System.out.println("How many numbers do you wish to input?");
+    int input2 = 0;
+     input2 = scan.nextInt();
+         int count = 0;
+         int largest_number = 0;
+    while (count <= input2)
+    {
+       System.out.println("Enter a Score");
+       input3 = scan.nextInt();
+       if (largest_number <= input3)
+        {
+          largest_number = input3;
+        }
+        count++;
+        }
+    }
   }
-}
+
